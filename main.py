cac115d3a0a6f69e9eaa53ab9241e468154b48ca
mysteo91 <mysteo91work@rambler.ru>
2022-01-12 12:27:13 +0300

    --2 vers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
+import torch
 import mqtt
-
+from facenet_pytorch import MTCNN
 
 import face_recognition
 import os
@@ -60,19 +61,19 @@ print('Loading known faces...')
 known_faces = []
 known_names = []
 
-for name in os.listdir(KNOWN_FACES_DIR):
-
-    # Next we load every file of faces of known person
-    for filename in os.listdir(f'{KNOWN_FACES_DIR}/{name}'):
-        # Load an image
-        image = face_recognition.load_image_file(f'{KNOWN_FACES_DIR}/{name}/{filename}')
-        # Get 128-dimension face encoding
-        # Always returns a list of found faces, for this purpose we take first face only (assuming one face per image as you can't be twice on one image)
-        encoding = face_recognition.face_encodings(image)[0]
-
-        # Append encodings and name
-        known_faces.append(encoding)
-        known_names.append(name)
+# for name in os.listdir(KNOWN_FACES_DIR):
+#
+#     # Next we load every file of faces of known person
+#     for filename in os.listdir(f'{KNOWN_FACES_DIR}/{name}'):
+#         # Load an image
+#         image = face_recognition.load_image_file(f'{KNOWN_FACES_DIR}/{name}/{filename}')
+#         # Get 128-dimension face encoding
+#         # Always returns a list of found faces, for this purpose we take first face only (assuming one face per image as you can't be twice on one image)
+#         encoding = face_recognition.face_encodings(image)[0]
+#
+#         # Append encodings and name
+#         known_faces.append(encoding)
+#         known_names.append(name)
 
 ################################################# PREPARE OPEN CV#################################
 
@@ -86,22 +87,33 @@ for name in os.listdir(KNOWN_FACES_DIR):
 
 
 # Capture frame-by-frame
-#cap = VideoCapture("rtsp://rtsp:aK973pS44@192.168.0.55:554/av_stream/ch0")
-cap = VideoCapture(0)
+cap = VideoCapture("rtsp://rtsp:aK973pS44@192.168.0.55:554/av_stream/ch0")
+#cap = VideoCapture(0)
 start_frame_number = 1
 timing = time.time()
-
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+mtcnn = MTCNN(keep_all=True, device=device)
 while True:
     frame = cap.read()
-    frame = cv2.resize(frame, (240, 180))
-    cv2.imshow('rez', frame)
-    image = frame
-    # This time we first grab face locations - we'll need them to draw boxes
-    locations = face_recognition.face_locations(image, model=MODEL)
+    frame = cv2.resize(frame, (320, 240))
+    image = cap.read()
+    image = cv2.resize(image, (324, 240))
 
+    boxes, _ = mtcnn.detect(image)
+    if not (boxes is None):
+        for box in boxes:
+            cv2.rectangle(image, (round(box[0]), round(box[1])), (round(box[2]), round(box[3])), (255, 255, 0))
+    cv2.imshow('rez', image)
+
+    # This time we first grab face locations - we'll need them to draw boxes
+    locations = face_recognition.face_locations(frame, model=MODEL)
+    if len(locations) > 0:
+        cv2.rectangle(frame, (locations[0][3], locations[0][2]), (locations[0][1], locations[0][0]), (255, 255, 0))
+    cv2.imshow('rez2', frame)
     # Now since we know loctions, we can pass them to face_encodings as second argument
     # Without that it will search for faces once again slowing down whole process
-    encodings = face_recognition.face_encodings(image, locations)
+    #encodings = face_recognition.face_encodings(image, locations)
+    encodings = []
 
     # We passed our image through face_locations and face_encodings, so we can modify it
     # First we need to convert it from RGB to BGR as we are going to work with cv2
@@ -109,33 +121,33 @@ while True:
 
     # But this time we assume that there might be more faces in an image - we can find faces of dirrerent people
 
-    if time.time() - timing > TIME_FOR_REPEAT:
-        mqtt.publish(mqtt_client, 'online', 'camera/online')
-        TIME_FOR_REPEAT = 0.4
-        timing = time.time()
-        print(f', found {len(encodings)} face(s)')
-        if len(encodings) == 0:
-            mqtt.publish(mqtt_client, 'no', 'camera/match')
-        else:
-            for face_encoding, face_location in zip(encodings, locations):
-
-                # We use compare_faces (but might use face_distance as well)
-                # Returns array of True/False values in order of passed known_faces
-                # results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE)
-                results = face_recognition.face_distance(known_faces, face_encoding)
-                best_match_index = np.argmin(results)
-                match = None
-                if results[best_match_index] <= TOLERANCE:
-                    match = known_names[best_match_index]
-                    mqtt.publish(mqtt_client, 'yes', 'camera/match')
-                    mqtt.publish(mqtt_client, match, 'camera/face')
-                    TIME_FOR_REPEAT = 3
-
-                else:
-                    match = "Unknown"
-                    mqtt.publish(mqtt_client, 'no', 'camera/match')
-                    print(f' - {match} from UNKNOWN')
-                    print(face_location)
+    # if time.time() - timing > TIME_FOR_REPEAT:
+    #     mqtt.publish(mqtt_client, 'online', 'camera/online')
+    #     TIME_FOR_REPEAT = 0.4
+    #     timing = time.time()
+    #     print(f', found {len(encodings)} face(s)')
+    #     if len(encodings) == 0:
+    #         mqtt.publish(mqtt_client, 'no', 'camera/match')
+    #     else:
+    #         for face_encoding, face_location in zip(encodings, locations):
+    #
+    #             # We use compare_faces (but might use face_distance as well)
+    #             # Returns array of True/False values in order of passed known_faces
+    #             # results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE)
+    #             results = face_recognition.face_distance(known_faces, face_encoding)
+    #             best_match_index = np.argmin(results)
+    #             match = None
+    #             if results[best_match_index] <= TOLERANCE:
+    #                 match = known_names[best_match_index]
+    #                 mqtt.publish(mqtt_client, 'yes', 'camera/match')
+    #                 mqtt.publish(mqtt_client, match, 'camera/face')
+    #                 TIME_FOR_REPEAT = 3
+    #
+    #             else:
+    #                 match = "Unknown"
+    #                 mqtt.publish(mqtt_client, 'no', 'camera/match')
+    #                 print(f' - {match} from UNKNOWN')
+    #                 print(face_location)
 
 
 
